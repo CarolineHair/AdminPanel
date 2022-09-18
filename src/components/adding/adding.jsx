9f8d9d540044291e2b1eaafc380c3d3ec423f7c1
smallbenji <smallbenji@LenWin.localdomain>
2022-09-18 13:34:35 +0200

    added creating new times for booking

--- src/components/adding/adding.jsx
+++ src/components/adding/adding.jsx
@@ -1,21 +1,26 @@
 import { React, useState} from 'react'
-import {TextField} from '@mui/material'
+import {Button, TextField} from '@mui/material'
 import './adding.scss'
-import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import DateTimePicker from 'react-datetime-picker';
+
 
 export default function Adding() {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState(new Date());
+    const [name, setName] = useState();
+
+    const fetchAddTime = (name, value) => {
+        fetch(`https://carolinehair.herokuapp.com/addTime/${name}/${value}`)
+    };
   return (
     <div className='Adding'>
         <div className='box'>
             <h1>Tilføj tid:</h1>
-            <TextField label="titel"></TextField>
-            <LocalizationProvider>
-                <DateTimePicker 
-                    label="Date&Time picker"
-
-                />
-            </LocalizationProvider>
+            <TextField label="titel" className='TextField' onChange={setName}></TextField>
+            <br />
+            <DateTimePicker onChange={setValue} format='HH:mm dd-MM-y' value={value} className='TimePicker' />
+            <br />
+            <Button variant='contained' size='large' className="submitButton" onClick={() => {fetchAddTime(name.target.value, value.getTime())}}>Tilføj tid</Button>
         </div>
     </div>
   )
